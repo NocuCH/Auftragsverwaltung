7de9d612e504f51a427b6406336cc400aebb3533
Tim Hutter <huttertim13.gmail.com@zbw-online.ch>
2024-03-14 17:49:14 +0100

    Utils Class

---
+++ Auftragserfassung/Utils.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Auftragserfassung
+{
+    internal static class Utils
+    {
+        public static void WindowFunctionSelect()
+        {
+
+        }
+
+        public static void TemporalSelect()
+        {
+        
+        }
+
+        public static void cteSelect()
+        {
+
+        }
+    }
+}
